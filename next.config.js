45abd24e75625a1aaa6d779a37f5717f6ba6b25a
Agustin Di Benedetto <agustindibenedetto@hotmail.com>
2023-03-23 11:10:49 -0300

    added image domain

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost', '192.168.0.210']
+    domains: ['localhost', '192.168.0.210', 'https://b4b-deck-maker.onrender.com']
   }
 }
 
